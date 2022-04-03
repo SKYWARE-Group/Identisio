20d074bfa3850a31098d042c92bb4f0a463e93aa
Mikhail Kalatchev <kalatchev@hotmail.com>
2022-04-03 16:31:37 +0300

    UIN

---
+++ Identisio/Personal/Bg/Uin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identisio.Personal.Bg
+{
+    public class Uin : IdentifierBase
+    {
+
+        public override string Name => "Unique idenifier of a docotr";
+
+        public override string NativeAbbreviation => "УИН";
+
+        public override string NativeName => "Уникален идентификцаионен номер на лекар";
+
+        public override bool IsPersonalData => false;
+
+        public static bool Validate(string value) => throw new NotImplementedException();
+
+    }
+
+}
