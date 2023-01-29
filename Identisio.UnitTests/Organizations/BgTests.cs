ba1d950f84f56586ce4b2d928659bd757c2a6c8b
Aleko Neshev <alekonesh@gmail.com>
2023-01-29 15:56:38 +0200

    Unit test done for all eik validation cases.

--- Identisio.UnitTests/Organizations/BgTests.cs
+++ Identisio.UnitTests/Organizations/BgTests.cs
@@ -10,17 +10,31 @@ namespace Identisio.UnitTests.Organizations
 {
     public class BgTests
     {
-        [Test()]
-        public void EikValidateNine()
+
+        [TestCase("121708719")]
+        [TestCase("202557570")]              
+        [TestCase("1751162860016")]
+        [TestCase("1751162861128")]
+        [TestCase("1217087192000")]
+        public void EikValidateTrue(string eik)
         {
-            var eikValidate = Eik.Validate("121708719");
-            Assert.IsTrue(eikValidate);
+            var eikValidate = Eik.Validate(eik);
+            Assert.That(eikValidate, Is.True);
         }
-        [Test()]
-        public void EikValidateThirteen()
+        [TestCase("")]
+        [TestCase("20255757")]
+        [TestCase("121708729")] 
+        [TestCase("175116280")]
+        [TestCase("202557571")]
+        [TestCase("1751162860013")]
+        [TestCase("1751162861127")]
+        [TestCase("1217087192001")]
+        [TestCase("1017087192000")]
+        public void EikValidateFalse(string eik) 
         {
-            var eikValidate = Eik.Validate("1751162860016");
-            Assert.IsTrue(eikValidate);
+            var eikValidate = Eik.Validate(eik);
+            Assert.That(eikValidate, Is.False);
         }
+       
     }
 }
