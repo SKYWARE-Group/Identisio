fa8830aba1ce79afb238d34f2a17160747683ec4
Иван Калъчев <mani.munana@gmail.com>
2024-01-02 00:37:10 +0200

    fluentvalidation and versions and tests

---
+++ Identisio.FluentValidation.Tests/Model/IdentifierTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identisio.FluentValidation.Tests.Model;
+
+public enum IdentifierTypes : int
+{
+
+    Egn = 1,
+    Lnch = 2,
+    YuPid = 3
+
+}
--- Identisio.FluentValidation/FluentValidationExtensions.cs
+++ Identisio.FluentValidation/FluentValidationExtensions.cs
@@ -11,19 +11,19 @@ namespace Skyware.Identisio.FluentValidation;
 public static class FluentValidationExtensions
 {
 
-    public static IRuleBuilderOptions<T, string> IsValidEgn<T>(this IRuleBuilder<T, string> ruleBuilder) => 
+    public static IRuleBuilderOptions<T, string?> IsValidEgn<T>(this IRuleBuilder<T, string> ruleBuilder) => 
         ruleBuilder.SetValidator(new IdentifierValidator<Egn>());
 
-    public static IRuleBuilderOptions<T, string> IsValidLnch<T>(this IRuleBuilder<T, string> ruleBuilder) =>
+    public static IRuleBuilderOptions<T, string?> IsValidLnch<T>(this IRuleBuilder<T, string> ruleBuilder) =>
         ruleBuilder.SetValidator(new IdentifierValidator<Lnch>());
 
-    public static IRuleBuilderOptions<T, string> IsValidYuPid<T>(this IRuleBuilder<T, string> ruleBuilder) =>
+    public static IRuleBuilderOptions<T, string?> IsValidYuPid<T>(this IRuleBuilder<T, string> ruleBuilder) =>
         ruleBuilder.SetValidator(new IdentifierValidator<YuPid>());
 
-    public static IRuleBuilderOptions<T, string> IsValidEik<T>(this IRuleBuilder<T, string> ruleBuilder) =>
+    public static IRuleBuilderOptions<T, string?> IsValidEik<T>(this IRuleBuilder<T, string> ruleBuilder) =>
         ruleBuilder.SetValidator(new IdentifierValidator<Eik>());
 
-    public static IRuleBuilderOptions<T, string> IsValidIban<T>(this IRuleBuilder<T, string> ruleBuilder) =>
+    public static IRuleBuilderOptions<T, string?> IsValidIban<T>(this IRuleBuilder<T, string> ruleBuilder) =>
         ruleBuilder.SetValidator(new IdentifierValidator<Iban>());
 
 }
